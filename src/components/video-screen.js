da9c1efedf79514ac3ee7fc6753f706f89e1ccfb
Guilherme <guilherme.rosa@redes.unb.br>
2020-05-08 12:28:26 -0300

    Get user display

--- src/components/video-screen.js
+++ src/components/video-screen.js
@@ -2,8 +2,10 @@ import React, { useState, useEffect, useRef } from 'react'
 
 function VideoScreen() {
   const [stream, setStream] = useState()
+  const [displayStream, setDisplayStream] = useState()
 
   const videoElm = useRef()
+  const displayElm = useRef()
 
   useEffect(() => {
     navigator.mediaDevices.getUserMedia({
@@ -13,6 +15,13 @@ function VideoScreen() {
         height: 720
       }
     }).then(stream => setStream(stream))
+
+    navigator.mediaDevices.getDisplayMedia({
+      video: {
+        cursor: 'always' | 'motion' | 'never',
+        displaySurface: 'application' | 'browser' | 'monitor' | 'window'
+      }
+    }).then(stream => setDisplayStream(stream))
   }, [])
 
   useEffect(() => {
@@ -21,12 +30,25 @@ function VideoScreen() {
     }
   }, [stream])
 
+  useEffect(() => {
+    if (displayElm.current) {
+      displayElm.current.srcObject = displayStream
+    }
+  }, [displayStream])
+
   return (
-    <video
-      autoPlay
-      muted
-      ref={videoElm}
-    ></video>
+    <>
+      <video
+        autoPlay
+        muted
+        ref={videoElm}
+      ></video>
+      <video
+        autoPlay
+        muted
+        ref={displayElm}
+      ></video>
+    </>
   )
 }
 
